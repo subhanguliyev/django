4ec34554b2aee57e223d559af52c866917d428c5
Subhan <squadiloglu@gmail.com>
2022-10-29 16:32:16 +0400

    created main/urls, modif main/views

--- aquaman/main/views.py
+++ aquaman/main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse('<h4>WELCOME TO AQUAMAN</h4>')
+
+
+def about(request):
+    return HttpResponse('<h4>ABOUT AQUAMAN</h4>')
