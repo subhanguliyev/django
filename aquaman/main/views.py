e25becfc3c3d35d4c34a92a395b20a92c0741536
Subhan <squadiloglu@gmail.com>
2022-11-07 17:43:12 +0400

    add index,about htmls

--- aquaman/main/views.py
+++ aquaman/main/views.py
@@ -4,8 +4,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('<h4>WELCOME TO AQUAMAN</h4>')
+    return render(request, 'main/index.html')
 
 
 def about(request):
-    return HttpResponse('<h4>ABOUT AQUAMAN</h4>')
+    return render(request, 'main/about.html')
